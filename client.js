75cb0b1d7a6d02ddff0e11480c295ac787e492a8
salman shaik <dukesalman5@gmail.com>
2018-01-02 12:59:54 +0530

    refactored the code

--- client.js
+++ client.js
@@ -5,17 +5,21 @@ var rl=readline.createInterface({
    output: process.stdout
 });
 var client = new net.connect({port:8000, host:'localhost'}, function() {
-	console.log('Connected');
-	rl.on("line",(input)=>{
-		client.write(`${input}`);
-	})
+  console.log('Connected');
+  rl.on("line",(input)=>{
+  client.write(`${input}`);
+ })
 });
 
 
 client.on('data', function(data) {
-	console.log(`${data.toString()}`);
+	console.log(`${data}`);
+});
+
+client.on('end', function() {
+	console.log('Connection Closed');
 });
 
 client.on('close', function() {
-	console.log('Connection closed');
+	console.log('Disconnected From Server');
 });
--- server.js
+++ server.js
@@ -8,19 +8,23 @@ const requestHandler=(socket) => {
 
   socket.name = socket.remoteAddress + ":" + socket.remotePort
   socket.name=socket.name.slice(socket.name.indexOf('f:')+2);
-
+  if(!clients.includes(socket)){
   clients.push(socket);
+   }
+   else{
+     displayOnConsole(`${socket.name} has Taken`,socket)
+   }
 
   socket.write("Welcome " + socket.name + "\n");
-  displayOnConsole(socket.name + " joined the chat\n", socket);
+  displayOnConsole(`${socket.name} joined the chat`,socket);
 
   socket.on('data', function (data) {
-    displayOnConsole(socket.name + " > " + data, socket);
+    displayOnConsole(`${socket.name} > ${data}`, socket);
   });
 
   socket.on('end', function () {
     clients.splice(clients.indexOf(socket), 1);
-    displayOnConsole(socket.name + " left the chat.\n");
+    displayOnConsole(`${socket.name} left the chat.`,socket);
   });
 
 };
@@ -28,9 +32,9 @@ const requestHandler=(socket) => {
 const displayOnConsole=function(message, sender) {
   clients.forEach(function (client) {
     if (client === sender) return;
-    client.write(message);
+    client.write(`${message}\n`);
   });
-  process.stdout.write(message+'\n')
+  process.stdout.write(`${message}\n`)
 }
 
 let server=net.createServer(requestHandler)
