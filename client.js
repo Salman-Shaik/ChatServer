4667742c94ab715b0c2af9a939a21958c280b130
salman shaik <dukesalman5@gmail.com>
2018-01-03 11:20:14 +0530

    made source code tidy

--- client.js
+++ client.js
@@ -1,25 +1,27 @@
-var net = require('net');
-var readline=require('readline');
-var rl=readline.createInterface({
+let net = require('net');
+let readline=require('readline');
+let rl=readline.createInterface({
    input: process.stdin,
    output: process.stdout
 });
-var client = new net.connect({port:8000, host:'localhost'}, function() {
+let client = new net.connect({port:8000, host:'localhost'}, function() {
   console.log('Connected');
   rl.on("line",(input)=>{
   client.write(`${input}`);
  })
 });
 
-
 client.on('data', function(data) {
 	console.log(`${data}`);
 });
 
 client.on('end', function() {
-	console.log('Connection Closed');
-});
-
-client.on('close', function() {
 	console.log('Disconnected From Server');
 });
+
+client.on('error',function (e){
+  if(e.code=='ECONNREFUSED'){
+    console.log('Server Not Ready');
+  }
+  client.destroy();
+})
--- server.js
+++ server.js
@@ -4,40 +4,38 @@ let net = require('net');
 
 var clients = [];
 
-const requestHandler=(socket) => {
-
+const requestHandler = (socket) => {
   socket.name = socket.remoteAddress + ":" + socket.remotePort
-  socket.name=socket.name.slice(socket.name.indexOf('f:')+2);
-  if(!clients.includes(socket)){
-  clients.push(socket);
-   }
-   else{
-     displayOnConsole(`${socket.name} has Taken`,socket)
-   }
+  socket.name = socket.name.slice(socket.name.indexOf('f:') + 2);
+  if (!clients.includes(socket)) {
+    clients.push(socket);
+  } else {
+    displayOnConsole(`${socket.name} has Taken`, socket)
+  }
 
   socket.write("Welcome " + socket.name + "\n");
-  displayOnConsole(`${socket.name} joined the chat`,socket);
+  displayOnConsole(`${socket.name} joined the chat`, socket);
 
-  socket.on('data', function (data) {
+  socket.on('data', function(data) {
     displayOnConsole(`${socket.name} > ${data}`, socket);
   });
 
-  socket.on('end', function () {
+  socket.on('end', function() {
     clients.splice(clients.indexOf(socket), 1);
-    displayOnConsole(`${socket.name} left the chat.`,socket);
+    displayOnConsole(`${socket.name} left the chat.`, socket);
   });
 
 };
-
-const displayOnConsole=function(message, sender) {
-  clients.forEach(function (client) {
-    if (client === sender) return;
+const displayOnConsole = function(message, sender) {
+  clients.forEach(function(client) {
+    if (client === sender) {
+      return
+    };
     client.write(`${message}\n`);
   });
   process.stdout.write(`${message}\n`)
 }
 
-let server=net.createServer(requestHandler)
+let server = net.createServer(requestHandler);
 server.listen(PORT);
-
 console.log(`Chat server running at port ${PORT}\n`);
